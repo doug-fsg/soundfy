925eb2164389ca36433777bf3233053fd9d74521
doug-fsg <104734964+doug-fsg@users.noreply.github.com>
2024-08-10 14:29:33 -0300

    Update Model.js

--- src/models/Model.js
+++ src/models/Model.js
@@ -2,7 +2,7 @@ import { Model as BaseModel } from "vue-api-query";
 
 export default class Model extends BaseModel {
   baseURL() {
-    return process.env.URL || "http://localhost:4000";
+    return process.env.VUE_APP_URL || "http://localhost:4000";
   }
 
   request(config) {
